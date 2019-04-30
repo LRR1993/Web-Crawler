8908e0fb96304e1277c9feb67e5cfecd7a41c771
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-30 17:01:29 +0100

    adds a piechart functionality that uses the results of the backend data request

--- fe-util-scripts.js
+++ fe-util-scripts.js
@@ -2,9 +2,6 @@
 // impirt is not correct
 // import requestCrawl from './requestCrawl.js';
 
-import {GoogleCharts} from 'google-charts';
-
-GoogleCharts.load(drawChart);
 
 const inputField = [...document.getElementsByClassName('form')][0];
 const requestButton = [...document.getElementsByClassName('requestButton')][0];
@@ -13,46 +10,24 @@ requestButton.addEventListener('click', event => {
   event.preventDefault();
   console.log(`requested ${inputField.value} for crawling`);
   // const results = requestCrawl(inputField.value);
-  const results = [['Chart thing', 'Chart amount']['good', 4], ['bad', 6]];
+  const results = [['Chart thing', 'Chart amount'],['good', 4], ['bad', 6]];
   drawChart(results);
 });
 
-function drawChart(results) {
-  const data = GoogleCharts.api.visualization.arrayToDataTable(results);
-
-  //   const options = {
-  //     pieHole: 0.8,
-  //     pieSliceTextStyle: {
-  //         color: 'black',
-  //     },
-  //     slices: {
-  //         0: {color: '#7ec252'},
-  //         1: {color: '#a4ce57'},
-  //         2: {color: '#cfe4ad'}
-  //     },
-  //     legend: {
-  //         position: 'bottom',
-  //         textStyle: {
-  //             color: 'black',
-  //             fontSize: 13,
-  //             fontName: 'EncodeSans'
-  //         }
-  //     },
-  //     title: 'Chart 1',
-  //     titleTextStyle: {
-  //         color: 'black',
-  //         fontSize: 13,
-  //         fontName: 'EncodeSans'
-  //     },
-  //     chartArea: {left: 0, top: 0, width: '100%', height: '80%'},
-  //     pieSliceText: 'none'
-  // };
-  // advanced options
-
-  const chart = new GoogleCharts.api.visualization.PieChart(
-    document.getElementById('pieChart')
-  );
-  chart.draw(data);
-  // chart.draw(data, options);
-  // for the advanced option
+function drawChart (results) {
+  google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+  
+        function drawChart() {
+  
+          var data = google.visualization.arrayToDataTable(results);
+  
+          var options = {
+            title: 'My Daily Activities'
+          };
+  
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  
+          chart.draw(data, options);
+        }
 }
