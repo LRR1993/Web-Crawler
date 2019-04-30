c8164d1d96f704a49daed0c3c27e536065e56ca1
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-04-30 16:21:04 +0100

    regex added to get link from page

--- index.js
+++ index.js
@@ -9,35 +9,46 @@ http
   .get(options, res => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
-
-    console.log(res.headers);
-    // let error;
-    // if (statusCode !== 200) {
-    //   error = new Error('Request Failed.\n' +
-    //     `Status Code: ${statusCode}`);
-    // } else if (!/^application\/json/.test(contentType)) {
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+    }
+    // else if (!/^application\/json/.test(contentType)) {
     //   error = new Error('Invalid content-type.\n' +
     //     `Expected application/json but received ${contentType}`);
     // }
-    // if (error) {
-    //   console.error(error.message);
-    //   // Consume response data to free up memory
-    //   res.resume();
-    //   return;
-    // }
+    if (error) {
+      console.error(error.message);
+      res.resume();
+      return;
+    }
 
-    // res.setEncoding('utf8');
-    // let rawData = '';
-    // res.on('data', (chunk) => { rawData += chunk; });
-    // res.on('end', () => {
-    //   try {
-    //     const parsedData = JSON.parse(rawData);
-    //     console.log(parsedData);
-    //   } catch (e) {
-    //     console.error(e.message);
-    //   }
-    // });
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', chunk => {
+      rawData += chunk;
+    });
+    res.on('end', () => {
+      try {
+        const re = /<a\shref="\/([a-z0-9]*\.html)">/gi;
+        const linkArr = [];
+        let result;
+        while ((result = re.exec(rawData)) !== null) {
+          linkArr.push(result[1]);
+        }
+        console.log(linkArr);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
   })
   .on('error', e => {
     console.error(`Got error: ${e.message}`);
   });
+
+// if (linkArray.length < 0) {
+//   linkArray.forEach(link =>
+
+//     )
+
+// }
