6695cfeb6645a9d994319c19301f247774ac8a16
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-30 15:31:59 +0100

    combines html and script to grab the input value

--- fe-util-scripts.js
+++ fe-util-scripts.js
@@ -1,12 +1,12 @@
 // takes the input from the html and delivers it to the back-end crawler
+// impirt is not correct
+import requestCrawl from './requestCrawl.js'
 
 const inputField = [...document.getElementsByClassName("form")][0]
-const requestButton = [...document.getElementsByClassName("requestButton")]
+const requestButton = [...document.getElementsByClassName("requestButton")][0]
 
 requestButton.addEventListener("click", event => {
   event.preventDefault();
-  console.log("requested page for crawling");
-  requestCrawl(inputField);
+  console.log(`requested ${inputField.value} for crawling`);
+  requestCrawl(inputField.value);
 });
-
-function requestCrawl(url) {}
