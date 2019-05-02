6cfc104a24e963a524ba3bea575f86ce8cba93cc
badeyjohnson <ben.adey-johnson@arup.com>
2019-05-02 12:14:34 +0100

    browserify into bundle.js to run in browser

--- index.js
+++ index.js
@@ -1,65 +1,88 @@
-///http request
-
 const http = require('http');
-const fs = require('fs');
 
-const options = new URL('http://broken-links-api.herokuapp.com/');
-const badLinks = [];
+const inputField = [...document.getElementsByClassName("form")][0];
+const requestButton = [...document.getElementsByClassName("requestButton")];
 const goodLinks = [];
+const badLinks = [];
 
-http
-  .get(options, res => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-    }
+console.log(document.getElementsByClassName("form").item(0))
 
-    if (error) {
-      console.error(error.message);
-      res.resume();
-      return;
-    }
-
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', chunk => {
-      rawData += chunk;
-    });
-    res.on('end', () => {
-      try {
-        const re = /<a\shref="\/([a-z0-9]*\.html)">/gi;
-        const linkArr = [];
-        let result;
-        while ((result = re.exec(rawData)) !== null) {
-          linkArr.push(result[1]);
-        }
-
-        linkArr.forEach(link => {
-          http
-            .get(`${options}${link}`, res => {
-              if (res.statusCode !== 200) {
-                fs.appendFile('badlinks.txt', `${options}${link},`, err => {
-                  if (err) throw err;
-                  console.log('The "data to append" was appended to file!');
-                });
-              } else {
-                fs.appendFile('goodlinks.txt', `${options}${link},`, err => {
-                  if (err) throw err;
-                  console.log('The "data to append" was appended to file!');
-                });
-              }
-            })
-            .on('error', e => {
-              console.error(`Got error: ${e.message}`);
-            });
-        });
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  })
-  .on('error', e => {
-    console.error(`Got error: ${e.message}`);
+requestButton.addEventListener("click", event => {
+  event.preventDefault();
+  console.log(`requested ${inputField.value} for crawling`);
+  requestCrawl(inputField.value).then(() => {
+      console.log('bL', badLinks)
+      console.log('gL', goodLinks)
   });
+
+  const results = [["Chart thing", "Chart amount"], ["good", 4], ["bad", 6]];
+  drawChart(results);
+});
+
+function drawChart(results) {
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    const data = google.visualization.arrayToDataTable(results);
+    const options = {
+      title: "My Daily Activities"
+    };
+    const chart = new google.visualization.PieChart(
+      document.getElementById("piechart")
+    );
+    chart.draw(data, options);
+  }
+}
+
+function requestCrawl(url) {
+  http
+    .get(url, res => {
+      const { statusCode } = res;
+      let error;
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+      }
+  
+      if (error) {
+        console.error(error.message);
+        res.resume();
+        return;
+      }
+  
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const re = /<a\shref="\/([a-z0-9]*\.html)">/gi;
+          const linkArr = [];
+          let result;
+          while ((result = re.exec(rawData)) !== null) {
+            linkArr.push(result[1]);
+          }
+  
+          linkArr.forEach(link => {
+            http
+              .get(`${options}${link}`, res => {
+                if (res.statusCode !== 200) {
+                  badLinks.concat(`${options}${link}`)
+                } else {
+                  goodLinks.concat(`${options}${link}`)
+                }
+              })
+              .on('error', e => {
+                console.error(`Got error: ${e.message}`);
+              });
+          });
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
+    })
+    .on('error', e => {
+      console.error(`Got error: ${e.message}`);
+    });
+}
